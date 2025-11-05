ce6dbb9d315c7f2f45ddd3456204612302c412a7
Maxim Tyufyakov <maximtyufyakov@gmail.com>
2025-11-05 20:19:40 +0300

    first commit

---
+++ src/main/java/com/geo_surveys/geo_surveys_api/GeoSurveysApiApplication.java
@@ -0,0 +1,13 @@
+package com.geo_surveys.geo_surveys_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSurveysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoSurveysApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geo_surveys/geo_surveys_api/GeoSurveysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geo_surveys.geo_surveys_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoSurveysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
