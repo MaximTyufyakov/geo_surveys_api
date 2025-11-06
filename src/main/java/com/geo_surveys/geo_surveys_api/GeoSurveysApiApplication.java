e288e584f2ef921bd0f01f0091d8de27980e4099
Maxim Tyufyakov <maximtyufyakov@gmail.com>
2025-11-06 23:40:56 +0300

    Test app.

--- src/main/java/com/geo_surveys/geo_surveys_api/GeoSurveysApiApplication.java
+++ src/main/java/com/geo_surveys/geo_surveys_api/GeoSurveysApiApplication.java
@@ -1,13 +1,21 @@
 package com.geo_surveys.geo_surveys_api;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GeoSurveysApiApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(GeoSurveysApiApplication.class, args);
+    }
+    // @Value("${DB_PASSWORD}")
+    // private String dbPassword;
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeoSurveysApiApplication.class, args);
-	}
-
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
