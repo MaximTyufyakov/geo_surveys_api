7dcad54d5b53ddd3fbc1d8e0fc00f4db467e8d4a
Maxim Tyufyakov <maximtyufyakov@gmail.com>
2025-11-19 10:48:38 +0300

    Update TaskUpdateDto.

--- src/main/java/com/geo_surveys/geo_surveys_api/dto/update/TaskUpdateDto.java
+++ src/main/java/com/geo_surveys/geo_surveys_api/dto/update/TaskUpdateDto.java
@@ -1,7 +1,10 @@
 package com.geo_surveys.geo_surveys_api.dto.update;
 
+import com.geo_surveys.geo_surveys_api.dto.create.VideoCreateDto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
 
 /**
  * DTO for task update (request).
@@ -12,10 +15,16 @@ public record TaskUpdateDto(
         @Min(value = 1, message = "task_id должно быть больше 0")
         Long task_id,
 
-        @NotNull(message = "Отстутсвует поле completed")
-        Boolean completed,
+        @NotNull(message = "Отстутсвует поле updatedPoints")
+        List<PointUpdateDto> updatedPoints,
 
-        String report
+        String report,
+
+        @NotNull(message = "Отстутсвует поле createdVideos")
+        List<VideoCreateDto> createdVideos,
+
+        @NotNull(message = "Отстутсвует поле deletedVideos")
+        List<Long> deletedVideos
 ) {
 
 }
