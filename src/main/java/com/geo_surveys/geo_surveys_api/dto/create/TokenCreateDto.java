71263a711f41868657789d0f9e7c559bf498e9ed
Maxim Tyufyakov <maximtyufyakov@gmail.com>
2025-12-01 23:05:17 +0300

    @Size remove for login.

--- src/main/java/com/geo_surveys/geo_surveys_api/dto/create/TokenCreateDto.java
+++ src/main/java/com/geo_surveys/geo_surveys_api/dto/create/TokenCreateDto.java
@@ -9,7 +9,6 @@ import jakarta.validation.constraints.Size;
 public record TokenCreateDto(
 
         @NotBlank(message = "Логин не может быть пустым")
-        @Size(min = 3, max = 50, message = "Поле login должно содержать от 3 до 50 символов")
         String login,
 
         @NotBlank(message = "Пароль не может быть пустым")
